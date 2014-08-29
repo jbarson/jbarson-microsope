07929cbd392beb6451835b742b2e0858b5cf28b1
Jon Barson <jbarson@gmail.com>
2014-08-29 15:57:22 -0400

    10-5 Denormalized the number of comments into the post.

--- server/fixtures.js
+++ server/fixtures.js
@@ -11,13 +11,13 @@ if (Posts.find().count() === 0) {
     profile: { name: 'Sacha Greif' }
   });
   var sacha = Meteor.users.findOne(sachaId);
-
   var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
-    submitted: now - 7 * 3600 * 1000
+    submitted: now - 7 * 3600 * 1000,
+    commentsCount: 2
   });
 
   Comments.insert({
@@ -41,7 +41,8 @@ if (Posts.find().count() === 0) {
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://meteor.com',
-    submitted: now - 10 * 3600 * 1000
+    submitted: now - 10 * 3600 * 1000,
+    commentsCount: 0
   });
 
   Posts.insert({
@@ -49,6 +50,7 @@ if (Posts.find().count() === 0) {
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
-    submitted: now - 12 * 3600 * 1000
+    submitted: now - 12 * 3600 * 1000,
+    commentsCount: 0
   });
 }
